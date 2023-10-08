26bc06c571845be524f8d2dea117db79a2df378e
Viktor Sívek <sivv01@vse.cz>
2023-10-08 17:10:49 +0200

    billboard

--- ecommerce_project_store/app/(routes)/page.tsx
+++ ecommerce_project_store/app/(routes)/page.tsx
@@ -1,6 +1,17 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard("74133904-10e4-4c80-9e8e-c6ffef0d85c5")
     return ( 
-        <div>Hello Store</div>
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+            </div>
+        </Container>
     );
 }
  
