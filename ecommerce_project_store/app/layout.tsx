182ecc93319d03a92a82dee2c2a66fc8692b53a7
Viktor Sívek <sivv01@vse.cz>
2023-11-08 15:59:18 +0100

    cart

--- ecommerce_project_store/app/layout.tsx
+++ ecommerce_project_store/app/layout.tsx
@@ -2,6 +2,8 @@ import { Urbanist } from 'next/font/google'
 
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
+import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
 
 import './globals.css'
 
@@ -22,6 +24,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
