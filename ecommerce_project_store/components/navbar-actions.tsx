4c39ba9f6649c1c44b6b23ff70c41239ae554a89
Viktor Sívek <sivv01@vse.cz>
2024-02-19 12:21:58 +0100

    admin button

--- ecommerce_project_store/components/navbar-actions.tsx
+++ ecommerce_project_store/components/navbar-actions.tsx
@@ -21,9 +21,17 @@ const NavbarActions = () => {
         return null;
     }
 
+    const openAdminPanel = () => {
+      window.open('https://ecommerce-project-admin-one.vercel.app/', '_blank');
+    };
+
     return ( 
         <div className="ml-auto flex items-center gap-x-4">
-          
+          <Button onClick={openAdminPanel} className="flex items-center rounded-full bg-blue-400 px-4 py-2">
+            <span className="text-sm font-medium text-white">
+              Admin
+            </span>
+          </Button>
           <Button onClick={() => router.push('/cart')} className="flex items-center rounded-full bg-black px-4 py-2">
             <ShoppingBag
               size={20}
