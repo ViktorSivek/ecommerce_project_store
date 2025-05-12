383a060c33a26e601741bbd118f90952cb0d396f
ViktorSivek <randomucet02@gmail.com>
2025-05-12 15:51:17 +0200

    fix build

--- ecommerce_project_store/app/(routes)/cart/page.tsx
+++ ecommerce_project_store/app/(routes)/cart/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import Container from '@/components/ui/container';
-import useCart from '@/hooks/use-cart';
+import Container from "@/components/ui/container";
+import useCart from "@/hooks/use-cart";
 
-import Summary from './components/summary'
-import CartItem from './components/cart-item';
-
-export const revalidate = 0;
+import Summary from "./components/summary";
+import CartItem from "./components/cart-item";
 
 const CartPage = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -29,7 +27,9 @@ const CartPage = () => {
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
-              {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
+              {cart.items.length === 0 && (
+                <p className="text-neutral-500">No items added to cart.</p>
+              )}
               <ul>
                 {cart.items.map((item) => (
                   <CartItem key={item.id} data={item} />
@@ -41,7 +41,7 @@ const CartPage = () => {
         </div>
       </Container>
     </div>
-  )
+  );
 };
 
 export default CartPage;
