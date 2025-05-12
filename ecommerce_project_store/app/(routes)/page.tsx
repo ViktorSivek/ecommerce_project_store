f2b5b7d999cb0b9c818803ef1d8b3b58515c1a14
ViktorSivek <randomucet02@gmail.com>
2025-05-12 15:09:25 +0200

    fixing main issues

--- ecommerce_project_store/app/(routes)/page.tsx
+++ ecommerce_project_store/app/(routes)/page.tsx
@@ -7,18 +7,18 @@ import Container from "@/components/ui/container";
 export const revalidate = 0;
 
 const HomePage = async () => {
-    const products = await getProducts({ isFeatured: true });
-    const billboard = await getBillboard("1c9e2505-16d1-4455-b9bc-5f46de64afde")
-    return ( 
-        <Container>
-            <div className="space-y-10 pb-10">
-                <Billboard data={billboard} />
-                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    <ProductList title="Featured Products" items={products} />
-                </div>
-            </div>
-        </Container>
-    );
-}
- 
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("30fc6236-2173-4eac-b7cb-0fd88d5e8ac6");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
 export default HomePage;
